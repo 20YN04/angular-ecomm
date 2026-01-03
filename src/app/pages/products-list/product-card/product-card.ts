fa76a87b67fa038b76643107093b12634e310705
20YN04 <yentl.nerinckx@icloud.com>
2026-01-03 17:04:58 +0100

    add product card component and integrate it into products list

---
+++ src/app/pages/products-list/product-card/product-card.ts
@@ -0,0 +1,32 @@
+import { Component, input } from '@angular/core';
+import { products } from '../../../models/product.models';
+import { PrimaryButton } from "../../../components/primary-button/primary-button";
+
+@Component({
+  selector: 'app-product-card',
+  imports: [PrimaryButton],
+  template: `
+    <div class="bg-white shadow-md border-shadow rounded-xl p-6 flex flex-col relative">
+
+      <div class="mx-auto">
+        <img [src]="product().image" class="w-50 h-25 object-contain"/>
+        <div class="flex flex-col mt-2">
+          <span class="text-md font-bold">{{product().title}}</span>
+          <span class="text-sm">{{'€' + product().price}}</span>
+          <app-primary-button label='Add to Cart' class="mt-3"/>
+        </div>
+        <span class="absolute top-2 right-3 text-sm font-bold" [class]="product().stock ? 'text-green-500' : 'text-red-500'">
+          @if (product().stock) {
+            {{product().stock}} left
+          }@else {
+            out of stock
+          }
+        </span>
+      </div>
+    </div>
+  `,
+  styles: ``,
+})
+export class ProductCard {
+  product = input.required<products>();
+}
